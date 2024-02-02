b799e51dd6cc7827441502086e4604dda76a3e0a
Dipongkor-Roy <juniordipu@gmail.com>
2024-02-02 12:32:47 +0600

    carts data get done

--- index.js
+++ index.js
@@ -42,6 +42,17 @@ async function run() {
     })
 
     //carts operations
+    app.get('/carts',async(req,res)=>{
+      const email=req.query.email;
+      console.log(email)
+      if(!email){
+        res.send([]);
+        
+      }
+      const query= {email:email};
+        const result=await cartsCollection.find(query).toArray();
+        res.send(result)
+    })
     app.post('/carts',async(req,res)=>{
       const item=req.body;
       console.log(item)
