bb03020db4e0e02d9496715b983beb26e266cd7f
Dipongkor-Roy <juniordipu@gmail.com>
2024-02-17 12:23:34 +0600

    admin api making done

--- index.js
+++ index.js
@@ -42,7 +42,25 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
-
+    //users delete
+    app.delete('/users/:id',async (req,res)=>{
+      const id=req.params.id;
+      const query={_id: new ObjectId(id)};
+      const result=await userCollection.deleteOne(query);
+      res.send(result);
+    })
+    //admin api 
+    app.path('/users/admin/:id',async (req,res)=>{
+      const id=req.params.id;
+      const filter={_id: new ObjectId(id)};
+      const updateDoc={
+       $set:{
+        role:'admin'
+       }
+      }
+      const result=await userCollection.updateOne(filter,updateDoc);
+      res.send(result)
+    })
     app.get("/menu", async (req, res) => {
       const query = {};
       const cursor = menuCollection.find(query);
