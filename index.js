8ebd57c5fc3b0190884f19a0f68cd2e8fbe15ff5
Dipongkor-Roy <juniordipu@gmail.com>
2024-02-21 22:45:39 +0600

    api writing error sily mistake solve

--- index.js
+++ index.js
@@ -38,20 +38,55 @@ async function run() {
     });
     //middlewares
     const verifyToken=(req,res,next)=>{
-      console.log('inside verify token',req.headers);
+      console.log('inside verify token: ',req.headers.authorization);
       if(!req.headers.authorization){
-        return res.status(401).send({message:'forbiden acess s'})
+        return res.status(401).send({message:'forbiden acess'})
       }
       const token=req.headers.authorization.split(' ')[1];
-      
-      //next();
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=> {
+        if(err){
+          return res.status(401).send({message:'forbiden acess'})
+        }
+        req.decoded=decoded;
+        next(); //first verify then go
+      });
+
     }
+
+    //verify admin
+    const verifyAdmin=async(req,res,next)=>{
+      const email=req.decoded.email;
+      const query={email:email};
+      const user=await userCollection.findOne(query);
+      const isAdmin=user?.role==='admin';
+      if(!isAdmin){
+        return res.status(403).send({message:'forbiden Access'})
+      }
+      next();
+    }
+
     //user data create
-    app.get("/users",verifyToken,async (req, res) => {
+    app.get("/users",verifyToken,verifyAdmin,async (req, res) => {
       const query = {};
       const result = await userCollection.find(query).toArray();
       res.send(result);
     });
+    //to get admin
+    app.get('/users/admin/:email',verifyToken,async(req,res)=>{
+      const email = req.params.email;
+      if(email !== req.decoded.email){
+        return res.status(403).send.apply({message:'unauthorized access'})
+      }
+      const query={email:email};
+      const user=await userCollection.findOne(query);
+      let admin=false;
+      if(user){
+        admin=user?.role ==='admin';
+
+      }
+      res.send({admin})
+
+    })
     app.post("/users", async (req, res) => {
       const user = req.body;
       const query = { email: user.email };
@@ -63,14 +98,14 @@ async function run() {
       res.send(result);
     });
     //users delete
-    app.delete("/users/:id", async (req, res) => {
+    app.delete("/users/:id",verifyToken,verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await userCollection.deleteOne(query);
       res.send(result);
     });
     //admin api
-    app.patch("/users/admin/:id", async (req, res) => {
+    app.patch("/users/admin/:id",verifyToken,verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updateDoc = {
